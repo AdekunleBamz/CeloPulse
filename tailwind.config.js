1836ae707020e2d56c4b88761307382456b07d8a
AdekunleBamz <bams.kunle@gmail.com>
2026-04-20 18:33:38 +0100

    chore: add celo-blue brand color to tailwind theme

--- tailwind.config.js
+++ tailwind.config.js
@@ -11,6 +11,7 @@ module.exports = {
         'celo-green': '#35D07F',
         'celo-gold': '#FBCC5C',
         'celo-dark': '#2E3338',
+        'celo-blue': '#73DDFF',
         border: 'rgba(20, 184, 166, 0.2)',
       },
       fontFamily: {
