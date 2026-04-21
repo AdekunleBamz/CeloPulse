d47803ad1834eb1f005a396ff86fe6a8f76461a5
AdekunleBamz <bams.kunle@gmail.com>
2026-04-21 14:00:56 +0100

    feat: add CLAIM_COOLDOWN_MS constant

--- lib/constants.ts
+++ lib/constants.ts
@@ -72,3 +72,6 @@ export const COMPOUND_COOLDOWN_MS = 86_400_000
 
 /** Cooldown (ms) before a user can compound rewards again. */
 export const COMPOUND_COOLDOWN_MS = 86_400_000
+
+/** Cooldown (ms) before a user can claim rewards again. */
+export const CLAIM_COOLDOWN_MS = 86_400_000
