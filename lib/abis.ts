e0261ebfae38d47fab923ab83e39d15a1561605c
AdekunleBamz <bams.kunle@gmail.com>
2026-04-21 14:07:17 +0100

    feat: add WRITE_FUNCTION_NAMES constant to abis lib

--- lib/abis.ts
+++ lib/abis.ts
@@ -73,3 +73,5 @@ export const erc20ABI = [
   { inputs: [{ internalType: 'address', name: 'owner', type: 'address' }, { internalType: 'address', name: 'spender', type: 'address' }], name: 'allowance', outputs: [{ internalType: 'uint256', name: '', type: 'uint256' }], stateMutability: 'view', type: 'function' },
   { inputs: [{ internalType: 'address', name: 'spender', type: 'address' }, { internalType: 'uint256', name: 'amount', type: 'uint256' }], name: 'approve', outputs: [{ internalType: 'bool', name: '', type: 'bool' }], stateMutability: 'nonpayable', type: 'function' },
 ] as const
+
+export const WRITE_FUNCTION_NAMES = ['registerActivity', 'claimRewards', 'compoundRewards', 'activateBoost'] as const
