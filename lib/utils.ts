67f0db7b9d1324614fea5a4b473413f4ef16e14d
AdekunleBamz <bams.kunle@gmail.com>
2026-04-26 16:52:09 +0100

    add omit to strip keys from an object

--- lib/utils.ts
+++ lib/utils.ts
@@ -364,3 +364,10 @@ export function slugify(str: string): string {
 export function pick<T extends object, K extends keyof T>(obj: T, keys: K[]): Pick<T, K> {
   return keys.reduce((acc, k) => ({ ...acc, [k]: obj[k] }), {} as Pick<T, K>)
 }
+
+/** Omits specified keys from an object. */
+export function omit<T extends object, K extends keyof T>(obj: T, keys: K[]): Omit<T, K> {
+  const result = { ...obj }
+  keys.forEach(k => delete (result as Record<string, unknown>)[k as string])
+  return result as Omit<T, K>
+}
